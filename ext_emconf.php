0f52f751c03786c7068798096a6631353da5aeed
th-f <t.fischer@bueroparallel.de>
2019-08-03 00:11:08 +0200

    Fix for version number in dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.5-9.9.99',
+			'typo3' => '9.5.0-9.9.99',
 		),
 		'conflicts' => array(
 		),
