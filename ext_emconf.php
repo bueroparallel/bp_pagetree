5915184248437f9f8c530ecf26d0b1b586692ad4
th-f <t.fischer@bueroparallel.de>
2019-08-02 23:33:26 +0200

    Initial release

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,7 @@
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Faster Backend Pagetree',
 	'description' => 'Performance-optimized backend page tree for large TYPO3 v9 installations (> 10k pages)',
-	'category' => 'backend',
+	'category' => 'be',
 	'author' => 'bueroparallel',
 	'author_email' => 'kontakt@bueroparallel.de',
 	'author_company' => 'bueroparallel',
